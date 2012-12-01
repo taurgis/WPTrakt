afb7ca688bf092bbcae336dd37f323d0419c60ef
Thomas Theunen <taurgis@users.noreply.github.com>
2012-12-01 11:56:36 +0000

    Fixed bug with history.

--- WPtrakt/MainPage.xaml.cs
+++ WPtrakt/MainPage.xaml.cs
@@ -60,13 +60,6 @@ namespace WPtrakt
                         App.ViewModel.LoadData();
                     }
                 }
-                else
-                {
-                    if (this.MainPanorama.SelectedIndex == 2)
-                    {
-                        App.ViewModel.LoadHistoryData();
-                    }
-                }
             }
             catch (InvalidOperationException) { }
         }
